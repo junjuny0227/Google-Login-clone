5194f8a49f92e44efb49b265e01294790f6bb258
junjuny0227 <junjuny0227@gmail.com>
2024-08-02 21:28:07 +0900

    Error Resolution

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+import * as S from "./style";
+import Logo from "./components/logo";
 import "./style/global.css";
 
 function App() {
-  return;
+  return (
+    <S.Wrapper>
+      <S.contentBox>
+        <Logo />
+      </S.contentBox>
+    </S.Wrapper>
+  );
 }
 
 export default App;
---
+++ src/style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #f0f4f9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const contentBox = styled.div`
+  width: 1040px;
+  height: 402px;
+  background-color: white;
+  border-radius: 28px;
+  padding: 36px;
+`;
